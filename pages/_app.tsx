0062a9fde290c99545107f8bf35663eb40356075
Anuarbek Zhakhangir <zhaxa65@gmail.com>
2023-07-03 21:17:32 +0600

    upd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ThemeProvider, createTheme } from "@mui/material";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Prompt",
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Prompt:wght@100&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
